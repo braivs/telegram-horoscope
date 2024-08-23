80c8b3b04b0d555616640c7e551b155016891216
Briws <brightwiths@gmail.com>
2024-08-24 04:46:23 +0700

    basic structure

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,7 @@
+export const LanguageSwitcher = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/ZodiacCard.tsx
@@ -0,0 +1,7 @@
+export const ZodiacCard = () => {
+  return (
+    <div>
+      
+      </div>
+  )
+}
