d4e39bb90da34f725735bf34785e279b078d73c3
Briws <brightwiths@gmail.com>
2024-08-24 04:50:14 +0700

    added i18n, installed packages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,1 +1,41 @@
-export * from "./i18n";
+// src/i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: {
+      zodiac: {
+        aries: 'Aries',
+        taurus: 'Taurus',
+        // ... другие знаки
+      },
+      back: 'Back',
+      language: 'Language'
+    },
+  },
+  ru: {
+    translation: {
+      zodiac: {
+        aries: 'Овен',
+        taurus: 'Телец',
+        // ... другие знаки
+      },
+      back: 'Назад',
+      language: 'Язык'
+    },
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en', // язык по умолчанию
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
